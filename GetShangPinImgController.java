a72e1f7e008c21c6be20a885efeb969c968d5091
1755616537 <1755616537@qq.com>
2020-09-07 16:40:18 +0800

    找不到文件，输出默认，防止报错

--- GetShangPinImgController.java
+++ GetShangPinImgController.java
@@ -3,8 +3,6 @@ package com.dabaoyutech.lingshou.controller;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.dabaoyutech.lingshou.model.User;
-import com.dabaoyutech.lingshou.util.CookiesPrivateUtils;
-import com.dabaoyutech.lingshou.util.TokenPrivateUtils;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -23,46 +21,45 @@ public class GetShangPinImgController {
         //定义输出信息
         JSONObject Return = new JSONObject();
 
-
-        TokenPrivateUtils TokenPrivateUtils = new TokenPrivateUtils();
-//        生成Token
-        JSONObject payloadObject = new JSONObject();
-        payloadObject.put("username", "2");
-        payloadObject.put("code", "2");
-        payloadObject.put("time", String.valueOf(System.currentTimeMillis()));
-        String Token = TokenPrivateUtils.SetTokenSession(payloadObject,session);
-
-        CookiesPrivateUtils CookiesPrivateUtils = new CookiesPrivateUtils();
-////        设置Cookies值
-        JSONObject SetCookiesJSONObject = CookiesPrivateUtils.SetCookies(response,request, "token", Token);
-        if (!SetCookiesJSONObject.getString("errcode").equals("0")) {
-            return SetCookiesJSONObject;
-        }
-//        取Cookies值
-        JSONObject GetCookiesJSONObject = CookiesPrivateUtils.GetCookies(request);
-        if (!GetCookiesJSONObject.getString("errcode").equals("0")) {
-            return GetCookiesJSONObject;
-        }
-        JSONObject GetCookiesJSONObjectMsg = JSONObject.parseObject(GetCookiesJSONObject.getString("msg"));
-        String Cookietoken = GetCookiesJSONObjectMsg.getString("token");
-
-        //        验证Token
-        JSONObject TokenGetReturn = TokenPrivateUtils.GetTokenSession(Cookietoken,session);
-        if (!TokenGetReturn.getString("errcode").equals("0")) {
-            System.out.println("GetShangPinImg,Token3错误：" + TokenGetReturn.getString("msg"));
-        } else {
-            System.out.println("GetShangPinImg,Token2成功,返回String型json格式payload中间信息：" + TokenGetReturn.getString("msg"));
-        }
-
-        //接收请求参数中的token
+//        TokenPrivateUtils TokenPrivateUtils = new TokenPrivateUtils();
+////        生成Token
+//        JSONObject payloadObject = new JSONObject();
+//        payloadObject.put("username", "2");
+//        payloadObject.put("code", "2");
+//        payloadObject.put("time", String.valueOf(System.currentTimeMillis()));
+//        String Token = TokenPrivateUtils.SetToken(payloadObject,session);
+//
+//        CookiesPrivateUtils CookiesPrivateUtils = new CookiesPrivateUtils();
+//////        设置Cookies值
+//        JSONObject SetCookiesJSONObject = CookiesPrivateUtils.SetCookies(response,request, "token", Token);
+//        if (!SetCookiesJSONObject.getString("errcode").equals("0")) {
+//            return SetCookiesJSONObject;
+//        }
+////        取Cookies值
+//        JSONObject GetCookiesJSONObject = CookiesPrivateUtils.GetCookies(request);
+//        if (!GetCookiesJSONObject.getString("errcode").equals("0")) {
+//            return GetCookiesJSONObject;
+//        }
+//        JSONObject GetCookiesJSONObjectMsg = JSONObject.parseObject(GetCookiesJSONObject.getString("msg"));
+//        String Cookietoken = GetCookiesJSONObjectMsg.getString("token");
+//
+//        //        验证Token
+//        JSONObject TokenGetReturn = TokenPrivateUtils.GetToken(Cookietoken,session);
+//        if (!TokenGetReturn.getString("errcode").equals("0")) {
+//            System.out.println("GetShangPinImg,Token3错误：" + TokenGetReturn.getString("msg"));
+//        } else {
+//            System.out.println("GetShangPinImg,Token2成功,返回String型json格式payload中间信息：" + TokenGetReturn.getString("msg"));
+//        }
+//
+//        //接收请求参数中的token
         String token = requestBody.getString("token");
-
-        //判空token
-        if (StringUtils.isBlank(token)) {
-            Return.put("errcode", "1");
-            Return.put("msg", "请求参数不合法");
-            return Return;
-        }
+//
+//        //判空token
+//        if (StringUtils.isBlank(token)) {
+//            Return.put("errcode", "1");
+//            Return.put("msg", "请求参数不合法");
+//            return Return;
+//        }
 
 //        //校验token是否正确
 //        String adminToken = (String) session.getAttribute("adminToken");
@@ -104,11 +101,30 @@ public class GetShangPinImgController {
             type = "";
         }
 
-//        成功返回
-        Return.put("errcode", "0");
-        Return.put("msg", "");
-        Return.put("imgList", ShaoMiao(type));
+        JSONArray ShaoMiaoReturn= ShaoMiao(type);
+//        判空
+        if(ShaoMiaoReturn.isEmpty()){
+            Return.put("errcode", "2");
+            Return.put("msg", "找不到图片,已自动获取默认图片");
+            Return.put("imgList", MoRenImg());
+        }else{
+//            成功返回
+            Return.put("errcode", "0");
+            Return.put("msg", "");
+            Return.put("imgList", ShaoMiaoReturn);
+        }
 
+
+        return Return;
+    }
+
+//    默认静态图片地址
+    private JSONArray MoRenImg(){
+        JSONArray Return=new JSONArray();
+        JSONObject Return2=new JSONObject();
+        Return2.put("name","apple");
+        Return2.put("url","public/static/img/fruits/apple.jpg");
+        Return.add(Return2);
         return Return;
     }
 
@@ -131,8 +147,14 @@ public class GetShangPinImgController {
 
     //    扫描目录
     private JSONArray Getfilepath(String path, JSONArray Return) {
-        File rootDir = new File(path);
-//        检查一个是否是文件夹
+        File rootDir = null;
+        try {
+            rootDir= new File(path);
+        }catch (Exception ignored){
+            System.out.println("Getfilepath,文件不存在：" + path);
+        }
+//        判空=>检查一个是否是文件夹
+        assert rootDir != null;
         if (rootDir.getName().split("\\.").length != 1) {
             JSONObject a = new JSONObject();
             a.put("name", rootDir.getName());
@@ -145,10 +167,13 @@ public class GetShangPinImgController {
             Return.add(a);
         } else {
             String[] fileList = rootDir.list();
-            for (int i = 0; i < Objects.requireNonNull(fileList).length; i++) {
-                path = rootDir.getAbsolutePath() + "/" + fileList[i];
-                Getfilepath(path, Return);
+            if(fileList!=null){
+                for (int i = 0; i < Objects.requireNonNull(fileList).length; i++) {
+                    path = rootDir.getAbsolutePath() + "/" + fileList[i];
+                    Getfilepath(path, Return);
+                }
             }
+
         }
         return Return;
     }
